047a0e33d11a8ca1b3472a0f47432f375373d1b4
Nycolas <nycolas@teste.com>
2025-04-24 17:20:42 -0300

    criação do header e contagem regressiva

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+const dataDoEvento = new Date("apr 17, 2026 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    // document.getElementById('contador').innerText = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    document.getElementById('dia').innerText = `${diasAteOEvento}`
+    document.getElementById('hora').innerText = `${horasAteOEvento}`
+    document.getElementById('minuto').innerText = `${minutosAteOEvento}`
+    document.getElementById('segundo').innerText = `${segundosAteOEvento}`
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado`
+    }
+},1000);
